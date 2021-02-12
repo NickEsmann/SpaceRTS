4667700d737b2e285739f4be38e07eb91db0b483
Hoffe <christian.hoffmann.thomsen@gmail.com>
2021-02-12 12:34:15 +0100

    GO Fix

--- GameObject.cs
+++ GameObject.cs
@@ -7,29 +7,50 @@ using Microsoft.Xna.Framework.Content;
 
 namespace SpaceRTS
 {
-    abstract class GameObject
+    internal abstract class GameObject
     {
         protected Texture2D sprite;
         protected Vector2 position;
         protected Color color;
         protected Vector2 origin;
         protected float rotation;
-
+        protected int offsetX;
+        protected int offsetY;
 
         public GameObject()
         {
+        }
 
+        public virtual Rectangle Collision
+        {
+            get
+            {
+                return new Rectangle(
+                       (int)position.X + offsetX,
+                       (int)position.Y,
+                       (int)sprite.Width,
+                       (int)sprite.Height + offsetY
+                   );
+            }
         }
 
         public abstract void LoadContent(ContentManager content);
 
         public abstract void Update(GameTime gameTime);
 
-        public void Draw(SpriteBatch spriteBatch)
+        public virtual void Draw(SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(sprite, position, null, color, rotation, origin, 1, SpriteEffects.None, 1);
+            spriteBatch.Draw(sprite, position, null, color, 0f, Vector2.Zero, 1, SpriteEffects.None, 0f);
         }
 
+        public abstract void OnCollision(GameObject other);
 
+        public void CheckCollision(GameObject other)
+        {
+            if (Collision.Intersects(other.Collision))
+            {
+                OnCollision(other);
+            }
+        }
     }
 }
