31995a7e5b59f91cb63c8655d189946289bb83b5
NickEsmann <10726@student.eadania.dk>
2021-02-12 12:54:46 +0100

    SMOL bits of code

--- Mine.cs
+++ Mine.cs
@@ -1,10 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace SpaceRTS
 {
     class Mine
     {
+
+        private Texture2D mine;
+        private Vector2 
+
+
+
     }
 }
