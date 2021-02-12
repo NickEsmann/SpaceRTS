10af1ae2f2ebd8977d8efda3668ffb8adce9e375
Hoffe <christian.hoffmann.thomsen@gmail.com>
2021-02-12 12:14:47 +0100

    Headquarter

--- Headquarter.cs
+++ Headquarter.cs
@@ -1,10 +1,38 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace SpaceRTS
 {
-    class Headquarter
+    internal class Headquarter
     {
+        public int Lv;
+        private int GoldCapasity;
+        private int CurrentGold;
+        private Texture2D Sprite;
+
+        public Headquarter()
+        {
+        }
+
+        public void AddGold(int Gold)
+        {
+            if (CurrentGold <= GoldCapasity)
+            {
+                Lv++;
+                CurrentGold = 0;
+                GoldCapasity = CurrentGold * 2;
+            }
+            else
+                CurrentGold += Gold;
+        }
+
+        public void LoadContent(ContentManager contentManager)
+        {
+            Sprite = contentManager.Load<Texture2D>("scifiStructure_07");
+        }
     }
 }
